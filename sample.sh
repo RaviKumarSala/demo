b2454b2992b4246ebaf0ef3b6dbc2713380838ee
ravikumarsala <ravikumarsala12@gmail.com>
2023-10-04 10:49:30 +0000

    new change

--- sample.sh
+++ sample.sh
@@ -1,7 +1,9 @@
-#! bash filr
-
-########## files #########
-ls
-
-##### date ############
-timedatectl
+#!/bin/bash
+echo "Enter the filename: "
+read filename
+if [ -f "$filename" ]; then
+  echo "File $filename exists. Contents:"
+  cat "$filename"
+else
+  echo "File $filename does not exist."
+fi
