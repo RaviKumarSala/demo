0d78cf4deced279bf6ed39fa475e804b8d693783
RaviKumarSala <146910421+RaviKumarSala@users.noreply.github.com>
2023-10-09 08:36:36 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "Enter the filename: "
-read filename
-if [ -f "$filename" ]; then
-  echo "File $filename exists. Contents:"
-  cat "$filename"
-else
-  echo "File $filename does not exist."
-fi
+#####  List Files ######
+ls -ltr
+
+##### space ######
+free -h
+
+##### time #########
+timedatectl
